8bcb7a84bb1b83c7e10ab741840b661754fe4561
jbdzizou <jbdzizou@gmail.com>
2019-10-05 21:45:22 +0900

    show and index pases

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,26 +1,37 @@
 class PicsController < ApplicationController
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
- def index
- end
+  def index
+  end
 
- def new
-   @pic = Pic.new
- end
+  def new
+    @pic = Pic.new
+  end
 
- def create
-   @pic = Pic.new(pic_params)
- end
- 
- def edit
- end
- 
- def show
- end
+  def show
+  end
 
- private
+  def create
+    @pic = Pic.new(pic_params)
 
- def pic_params
-   params.require(:pic).permit(:title, :description)
- end
+    if @pic.save
+      redirect_to @pic, notice: "yes, posted!!"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def pic_params
+    params.require(:pic).permit(:title, :description)
+  end
+
+  def find_pic
+    @pic = Pic.find(params[:id])
+  end
 
 end
